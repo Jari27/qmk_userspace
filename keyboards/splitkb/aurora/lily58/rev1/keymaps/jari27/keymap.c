a6d1067d8e99169be7805049a861af7fa9e027c5
Jari Meevis <jarimeevis@gmail.com>
2025-04-14 18:32:33 +0200

    feat(keymap): remove escape
    Removes the ESC single touch from the LCTL button because I use
    CTRL+<key> for autocomplete in neovim and I keep tapping <esc> instead.

--- keyboards/splitkb/aurora/lily58/rev1/keymaps/jari27/keymap.c
+++ keyboards/splitkb/aurora/lily58/rev1/keymaps/jari27/keymap.c
@@ -87,9 +87,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       [LAYER_DEFAULT] = LAYOUT(
           KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                              KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV,
           KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                              KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
-   LCTL_T(KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                              KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, RCTL_T(KC_QUOT),
+          KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                              KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, RCTL_T(KC_QUOT),
           KC_LSFT, Z_UNDO, X_CUT_,  C_COPY,   V_PASTE, KC_B, OSM(MOD_CAG),  OSM(MOD_HYPR),KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                           OSM(MOD_LALT), KC_LGUI, MO(LAYER_SYMBOLS), KC_SPC,             KC_ENT,  MO(LAYER_NAV),KC_BSPC, KC_RGUI
+                   OSM(MOD_LALT), KC_LGUI, MO(LAYER_SYMBOLS), KC_SPC,             KC_ENT,  MO(LAYER_NAV),KC_BSPC, KC_RGUI
       ),
       [LAYER_SYMBOLS] = LAYOUT(
           KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                             KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
